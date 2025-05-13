9ee1836e5411c85785a7e2b23074940a2158f5f1
arisa.inoue <arisa.inoue@nsw.co.jp>
2025-05-13 17:33:21 +0900

    feat: [v0.3.0] 初期画像もポップアップに表示されるように変更

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  const defaultImages = [
-    chrome.runtime.getURL("rakko.png") // 拡張機能内のデフォルト画像
-  ];
-  chrome.storage.sync.set({ images: defaultImages });
-});
